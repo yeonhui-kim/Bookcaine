fabcfff7d410d911441a06281e0fbb54396818bc
yeonhui-kim <76443750+yeonhui-kim@users.noreply.github.com>
2021-05-18 17:57:54 +0900

    Revert "Revert "로그아웃 처리""
    This reverts commit 457fb5dab463a33866e11e2d457f72c895f530de.

--- workspace/BookcainePrj/WebContent/js/login.js
+++ workspace/BookcainePrj/WebContent/js/login.js
@@ -3,15 +3,15 @@ window.addEventListener("load", function(){
 	var section = document.querySelector("login-info-form");
 	var loginButton = section.querySelector("login-button");
 	var idInput = section.querySelector("id-input");
-	var pwInput = section.querySelector("pw-input");
+	var pwdInput = section.querySelector("pwd-input");
 	
 	loginButton.onclick = function(e){
 		var id = idInput.value;
-		var pw = pwInput.value;
+		var pwd = pwdInput.value;
 		
 		if(id = null)
 			alert("아이디를 입력하세요");
-		if(pw = null)
+		if(pwd = null)
 			alert("비밀번호를 입력하세요");	
 	}
 	
