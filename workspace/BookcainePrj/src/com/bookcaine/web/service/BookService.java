95b36b421e20f51c6c7424bd208c45a7b80aa423
minjeongPark41 <pmjabcd@gmail.com>
2021-05-20 13:26:24 +0900

    interface 추가
    interface 추가

--- workspace/BookcainePrj/src/com/bookcaine/web/service/BookService.java
+++ workspace/BookcainePrj/src/com/bookcaine/web/service/BookService.java
@@ -1,136 +1,15 @@
 package com.bookcaine.web.service;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.bookcaine.web.entity.Book;
 
-public class BookService {	
-	
-	public List<Book> getList() throws ClassNotFoundException, SQLException{
-	
-		return getList(1, "title", "");
-	}
-	
-	
-	public List<Book> getList(String query) throws ClassNotFoundException, SQLException{
-		List<Book> list = new ArrayList<>();
-		
-		String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
-		// 필터링, 정렬, 그룹핑, ... -> SQL에서 담당
-		String sql = "SELECT * FROM BOOK WHERE TITLE LIKE '%" + query + "%' ORDER BY ID";
+public interface BookService {
 
-		Class.forName("oracle.jdbc.OracleDriver");
-		Connection con = DriverManager.getConnection(url, "BOOK", "12345");
-		
-		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery(sql);
-		
-		while(rs.next()) {
-			Book book = new Book();
-			int id = rs.getInt("id");
-			String title = rs.getString("title");
-			String author = rs.getString("author");
-			String yn = rs.getString("yn");
-			String details = rs.getString("details");
-			
-			book.setId(id);
-			book.setTitle(title);
-			book.setAuthor(author);
-			book.setYn(yn);
-			book.setDetails(details);
-			
-			list.add(book);
-		}
-		
-		rs.close();
-		st.close();
-		con.close();
-		
-		return list;
-	}
+	List<Book> getList();
+	List<Book> getList(String query);
+	List<Book> getList(int page, String field, String query);
+	Book get(int id);
 	
-	public List<Book> getList(int page, String field, String query) throws ClassNotFoundException, SQLException{
-		List<Book> list = new ArrayList<>();
-		
-		int size = 5;
-		int startN = 1+(page-1)*size;
-		int endN = page*size;
-		
-		String url	 = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
-		// 필터링, 정렬, 그룹핑, ... -> SQL에서 담당
-		String sql = "SELECT B.*, C.NAME "
-				+ "FROM BOOK B LEFT JOIN CATEGORY C "
-				+ "ON B.CATEGORY_ID = C.ID "
-				+ "WHERE "+field+" LIKE '%"+query+"%' "
-				+ "WHERE ID BETWEEN "+startN+" AND "+endN;
-
-		Class.forName("oracle.jdbc.OracleDriver");
-		Connection con = DriverManager.getConnection(url, "BOOK", "12345");
-		
-		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery(sql);
-		
-		while(rs.next()) {
-			Book book = new Book();
-			int id = rs.getInt("id");
-			String title = rs.getString("title");
-			String author = rs.getString("author");
-			String yn = rs.getString("yn");
-			String details = rs.getString("details");
-			String type = rs.getString("name");
-			
-			book.setId(id);
-			book.setTitle(title);
-			book.setAuthor(author);
-			book.setYn(yn);
-			book.setDetails(details);
-			book.setType(type);
-			
-			list.add(book);
-		}
-		
-		rs.close();
-		st.close();
-		con.close();
-		
-		return list;
-	}
 	
-	public Book get(int id) throws SQLException, ClassNotFoundException {
-		
-		Book book = new Book();
-		
-		String sql = "SELECT * FROM BOOK WHERE ID=" + id;
-		
-		String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
-		
-		Class.forName("oracle.jdbc.OracleDriver");
-		Connection con = DriverManager.getConnection(url, "BOOK", "12345");
-		
-		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery(sql);
-		
-		if(rs.next()) {
-			String title = rs.getString("title");
-			String author = rs.getString("author");
-			
-			book.setId(id);
-			book.setTitle(title);
-			book.setAuthor(author);
-		}
-
-		rs.close();
-		st.close();
-		con.close();
-
-		return book;
-		
-	}
-
 }
--- workspace/BookcainePrj/src/com/bookcaine/web/service/ReviewService.java
+++ workspace/BookcainePrj/src/com/bookcaine/web/service/ReviewService.java
@@ -1,76 +1,13 @@
 package com.bookcaine.web.service;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.bookcaine.web.entity.Review;
 
-public class ReviewService {
-	
-	public List<Review> getList(int bookId) throws ClassNotFoundException, SQLException{
-		List<Review> list = new ArrayList<>();
-		
-		String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
-		// 필터링, 정렬, 그룹핑, ... -> SQL에서 담당
-		String sql = "SELECT R.*, M.NICKNAME FROM REVIEW R LEFT JOIN MEMBER M ON R.WRITER_ID = M.ID WHERE BOOK_ID = " + bookId + " ORDER BY REGDATE DESC";
+public interface ReviewService {
 
-		Class.forName("oracle.jdbc.OracleDriver");
-		Connection con = DriverManager.getConnection(url, "BOOK", "12345");
-		
-		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery(sql);
-		
-		while(rs.next()) {
-			Review review = new Review();
-			int id = rs.getInt("id");
-			String writerId = rs.getString("writer_id");
-			String content = rs.getString("content");
-			String nickname = rs.getString("nickname");
-			
-			review.setId(id);
-			review.setWriterId(writerId);
-			review.setBookId(bookId);
-			review.setContent(content);
-			review.setNickname(nickname);
-			
-			list.add(review);
-		}
-		
-		rs.close();
-		st.close();
-		con.close();
-		
-		return list;
-	}
+	List<Review> getList(int bookId);
+	int insert(Review review);
 	
-	public int insert(Review review) throws ClassNotFoundException, SQLException {
-		int result = 0;
-		
-		String sql = "INSERT INTO REVIEW(ID, WRITER_ID, BOOK_ID, CONTENT) VALUES(?,?,?,?)";
-		
-		String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
-		Class.forName("oracle.jdbc.OracleDriver");
-		Connection con = DriverManager.getConnection(url, "BOOK", "12345");
-		
-		PreparedStatement st = con.prepareStatement(sql);
-		st.setInt(1, review.getId());
-		st.setString(2, review.getWriterId());
-		st.setInt(3, review.getBookId());
-		st.setString(4, review.getContent());
-		
-		result = st.executeUpdate(); // ex.Query():Select, ex.Update(): UPdate/Delete/Insert
-		// 업데이트된 개수를 반환
-		
-		st.close();
-		con.close();
-		
-		return result;
-	}
 	
 }
