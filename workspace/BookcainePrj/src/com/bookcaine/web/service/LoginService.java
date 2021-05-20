ee39ecff3f30d4ec1c8891a9d6be2dae0e9374c3
yeonhui-kim <76443750+yeonhui-kim@users.noreply.github.com>
2021-05-20 12:53:01 +0900

    Update LoginService.java
    로그인서비스 - 멤버list

--- workspace/BookcainePrj/src/com/bookcaine/web/service/LoginService.java
+++ workspace/BookcainePrj/src/com/bookcaine/web/service/LoginService.java
@@ -5,11 +5,41 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.bookcaine.web.entity.Member;
 
 public class LoginService {
+	
+	public List<Member> getList() throws ClassNotFoundException, SQLException {
+		
+		List<Member> list = new ArrayList<>();
+
+		String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
+		String sql = "SELECT * FROM MEMBER";
+		Class.forName("oracle.jdbc.OracleDriver");
+		Connection con = DriverManager.getConnection(url, "book", "12345");
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(sql);
+		
+		while(rs.next()) {
+			String id = rs.getString("id");
+			String pwd = rs.getString("pwd");
+			
+			Member member = new Member();
+			member.setId(id);
+			member.setPwd(pwd);
+			
+			
+			list.add(member);
+			
+		}
+		
+		return list;
+
+	}
 	
 	public int insert(Member member) throws ClassNotFoundException, SQLException {
 		int result = 0;
