becadce9d6d564ca14e85317170f02a071d780fe
yeonhui-kim <76443750+yeonhui-kim@users.noreply.github.com>
2021-05-28 02:13:35 +0900

    회원탈퇴 서비스
    실패!

--- workspace/BookcainePrj/src/com/bookcaine/web/controller/profile/SignoutController.java
+++ workspace/BookcainePrj/src/com/bookcaine/web/controller/profile/SignoutController.java
@@ -1,6 +1,7 @@
 package com.bookcaine.web.controller.profile;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.bookcaine.web.service.JdbcLoginService;
+import com.bookcaine.web.entity.Member;
 import com.bookcaine.web.service.JdbcSignoutService;
 
 @WebServlet("/sign/signout")
@@ -20,13 +21,38 @@ public class SignoutController extends HttpServlet {
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 		
+		
+		request.getRequestDispatcher("/WEB-INF/view/sign/signout.jsp").forward(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
 		HttpSession session = request.getSession();
 		
 		String id = request.getParameter("id");
 		String pwd = request.getParameter("pwd");
 		
-		JdbcSignoutService service = new JdbcSignoutService();
 		
-		request.getRequestDispatcher("/WEB-INF/view/sign/signout.jsp").forward(request, response);
+		
+		JdbcSignoutService service = new JdbcSignoutService();
+		try {
+			int check = service.deleteMember(id, pwd);
+			if(check == 1) {
+				session.invalidate();
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
 }
--- workspace/BookcainePrj/src/com/bookcaine/web/service/JdbcSignoutService.java
+++ workspace/BookcainePrj/src/com/bookcaine/web/service/JdbcSignoutService.java
@@ -6,34 +6,68 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.bookcaine.web.entity.Member;
 
 public class JdbcSignoutService {
 	
-	public Member deleteMember(String id, String pwd) throws ClassNotFoundException, SQLException {
+	@SuppressWarnings("resource")
+	public int deleteMember(String id, String pwd) throws ClassNotFoundException, SQLException {
 		
 		String url = "jdbc:oracle:thin:@hi.namoolab.com:1521/xepdb1";
-		String sql = "SELECT * FROM MEMBER WHERE ID = ? AND PWD = ?";
 		Class.forName("oracle.jdbc.OracleDriver");
 		
 		Connection con = null;
 		PreparedStatement st = null;
 		ResultSet rs = null;
 
-		Member deleteMember = null;
 		
+		int x = -1;
 		try {
-			con = DriverManager.getConnection(url, "book", "12345");
-			st = con.prepareStatement(sql);
+			//비밀번호 조회
+			StringBuffer query1 = new StringBuffer();
+            query1.append("SELECT PASSWORD FROM MEMBER WHERE ID=?");
+            //회원 삭제
+            StringBuffer query2 = new StringBuffer();
+            query2.append("DELETE FROM MEMBER WHERE ID=?");
+
+            con = DriverManager.getConnection(url, "book", "12345");
+            con.setAutoCommit(false);//자동커밋방지
+            
+            //아이디해당 비번조회
+			st = con.prepareStatement(query1.toString());
 			st.setString(1, id);
-			st.setString(2, pwd);
-			
 			rs = st.executeQuery();
 			
+			if(rs.next()) {
+				String dbpw = rs.getString("password");
+				if(dbpw.equals(pwd)) {//비번 같을 경우 회원삭제
+					st = con.prepareStatement(query2.toString());
+					st.setString(1, id);
+					st.executeUpdate();
+                    con.commit(); 
+                    x = 1; // 삭제 성공
+                } else {
+                    x = 0; // 비밀번호 비교결과 - 다름
+                }
+
+			}
+			return x;
 			
-				
+		}catch (Exception sqle) {
+            try {
+                con.rollback(); // 오류시 롤백
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            throw new RuntimeException(sqle.getMessage());
+        } finally {
+			try {
+				if(st != null) { st.close(); st = null;}
+				if(con != null) { con.close(); con = null;}
+			}catch(Exception e) {
+				throw new RuntimeException(e.getMessage());
+			}
 		}
-		
-		return deleteMember;
+
+
 	}
 }
